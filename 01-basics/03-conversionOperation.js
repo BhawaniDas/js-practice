5f9064940d64709270046b5000b16079f85d2575
BhawaniDas <bhawanidas05@gmail.com>
2026-02-01 07:57:14 +0000

    Conversion of Datatypes

---
+++ 01-basics/03-conversionOperation.js
@@ -0,0 +1,37 @@
+//let score = 33      //output - number
+//let score = "33"    //output - string
+// let score = "33abc"    //output - number
+// let score = null          //output - 0
+// let score = undefined    
+// let score = true          //output - 1
+// let score = false         //output - 0
+let score = "Bhawani"        //output - NaN
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);        //output -NaN (Not a Number)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isloggedIn = 1
+// let isloggedIn = ""
+let isloggedIn = "Bhawani"
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => ture; 0 => false
+// "" => false
+// "Bhawani" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
