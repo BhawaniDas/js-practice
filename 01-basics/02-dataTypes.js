6dc11b203c19cb4c2187f0e5e76a174f6a2a1a50
BhawaniDas <bhawanidas05@gmail.com>
2026-02-01 07:10:39 +0000

    Datatypes

---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";    //treat all JS code as newer version
+
+//alert (3+3)    //we r using JS not browser
+
+console.log(3
+    +3)   //code readability should be high
+
+console.log("Bhawani")
+
+
+let name = "Bhawani"
+let age = 20
+let isloggedIn = false
+let state;   //it will show undefined not null
+
+//number => 2 to power 53 (Range)
+//bigint => used for long no. like bitcoins etc.
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol => used for uniqueness
+
+
+//object
+
+console.log(typeof undefined);      //output = undefined
+console.log(typeof null);           //output = object
