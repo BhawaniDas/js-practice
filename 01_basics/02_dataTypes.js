1c64a0ce05c51381b51fa81f46528382f6aeeba6
BhawaniDas <bhawanidas05@gmail.com>
2026-03-09 10:23:45 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict";    //--> used to treat all JS code as newer vesion
+
+// alter("Hello")   --> we r using node.js, not browser and alter is used in browser mostly for arthmatic operation
+
+// FOR READABILITY -- coz code readability should be high
+console.log(3 + 3)
+console.log("Bhawani")
+
+let name = "Ashish"       //--> String DataType
+let age = 21              //--> Number DataType
+let isLoggedIn = false    //--> Boolean DataType
+
+
+// number => 2 to power 53 (Range)
+
+// bigint (mostly used in Trading, Stock Market or On Big Websites)
+
+// string => ""
+
+// boolean => true/false
+
+// null => standalone value 
+
+// undefined =>
+
+// symbol (used to find uniqueness, mostly used in React)
+
+
+// object 
+
+console.log(typeof "Bhawani");
+console.log(typeof age);
+console.log(typeof null);   //--> output = Object coz null is a object
+console.log(typeof undefined);     //--> output = undefined
