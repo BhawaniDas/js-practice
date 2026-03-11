88f7a6672149d5d6519c4ed8bf30374e24800bac
BhawaniDas <bhawanidas05@gmail.com>
2026-03-11 08:10:48 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -35,3 +35,36 @@ let stringNumber = String(someNumber)
 
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+
+// ********************* Operations **********************
+
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Bhawani"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);         //--> 12
+console.log(1 + "2");         //--> 12
+console.log("1" + "2");       //--> 12
+console.log("1" + 2 + 2);     //--> 122
+console.log(1 + 2 + "2");     //--> 32
+
+
+console.log(true);        //--> true
+console.log(+true);       //--> 1
+//console.log(true+);      //--> Error
